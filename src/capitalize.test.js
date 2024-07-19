9b0094bf57a52c75a8188e20c4e2a45b99319ad3
Jacob Dachenhaus <dachendev@gmail.com>
2024-07-18 22:21:38 -0400

    feat(capitalize): Add capitalize and corresponding tests

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,5 +1,31 @@
+import capitalize from "./capitalize";
+
 describe("capitalize", () => {
   test("exists", () => {
     expect(capitalize).toBeDefined();
+  });
+
+  const testCases = [
+    {
+      input: "hello",
+      actual: capitalize("hello"),
+      expected: "Hello",
+    },
+    {
+      input: "world",
+      actual: capitalize("world"),
+      expected: "World",
+    },
+    {
+      input: "foo",
+      actual: capitalize("foo"),
+      expected: "Foo",
+    },
+  ];
+
+  testCases.forEach((testCase) => {
+    test("first letter is capitalized", () => {
+      expect(testCase.actual).toBe(testCase.expected);
+    });
   });
 });
